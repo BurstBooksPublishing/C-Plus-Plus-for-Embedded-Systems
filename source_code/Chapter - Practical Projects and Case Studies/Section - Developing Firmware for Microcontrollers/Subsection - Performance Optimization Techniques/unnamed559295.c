0f724868ab8cbb784eb809e8c8ed51337a110830
BurstBooks <administrator@burstbooks.biz>
2025-10-23 16:56:50 +1300

    Upload source_code/Chapter - Practical Projects and Case Studies/Section - Developing Firmware for Microcontrollers/Subsection - Performance Optimization Techniques/unnamed559295.c

---
+++ source_code/Chapter - Practical Projects and Case Studies/Section - Developing Firmware for Microcontrollers/Subsection - Performance Optimization Techniques/unnamed559295.c
@@ -0,0 +1,10 @@
+void matrix_multiply(double A[N][N], double B[N][N], double C[N][N]) {
+for (int i = 0; i < N; i++) {
+for (int j = 0; j < N; j++) {
+C[i][j] = 0;
+for (int k = 0; k < N; k++) {
+C[i][j] += A[i][k] * B[k][j];
+}
+}
+}
+}
